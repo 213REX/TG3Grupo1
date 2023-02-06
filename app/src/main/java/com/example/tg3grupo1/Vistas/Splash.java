285a7db951c6b082a94288651f7c7172b1f92bcf
Nicolás Del Amo <nico.delamo.perez@gmail.com>
2023-02-06 16:32:05 +0100

    Splash(Codigo)

--- app/src/main/java/com/example/tg3grupo1/Vistas/Splash.java
+++ app/src/main/java/com/example/tg3grupo1/Vistas/Splash.java
@@ -2,7 +2,10 @@ package com.example.tg3grupo1.Vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.example.tg3grupo1.R;
 
@@ -10,8 +13,15 @@ public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.fondoInicio);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler(Looper.myLooper());
+        handler.postDelayed(()->{
+
+            Intent intent = new Intent(getApplicationContext(), Inicio.class);
+            startActivity(intent);
+            finish();
+        },5000);
     }
 }
