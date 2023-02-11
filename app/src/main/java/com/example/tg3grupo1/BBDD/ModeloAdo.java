c251a864ca2f241ed8e4e8a8d66c4004bf346456
Nicolás Del Amo <nico.delamo.perez@gmail.com>
2023-02-11 18:31:41 +0100

    consultas SQL (Codigo)
    se han creado metodos para buscar o actializar la base de datos

--- app/src/main/java/com/example/tg3grupo1/BBDD/ModeloAdo.java
+++ app/src/main/java/com/example/tg3grupo1/BBDD/ModeloAdo.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class ModeloAdo {
     private Context context;
-    private static final String NOMBRE = "nombre";
+    private static final String ID = "id";
 
     public ModeloAdo(Context context){
         this.context = context;
@@ -51,13 +51,13 @@ public class ModeloAdo {
             Cursor cursor = db.rawQuery(sql, new String[]{String.valueOf(id)});
 
             if (cursor.moveToNext()){
-                Modelo alumno = new Modelo();
-                alumno.setId(id);
-                alumno.setTitulo(cursor.getString(1));
-                alumno.setUltimaactualizacion(cursor.getString(2));
-                alumno.setCoordenadas(cursor.getString(3));
-                alumno.setIcono(cursor.getString(4));
-                return alumno;
+                Modelo parada = new Modelo();
+                parada.setId(id);
+                parada.setTitulo(cursor.getString(1));
+                parada.setUltimaactualizacion(cursor.getString(2));
+                parada.setCoordenadas(cursor.getString(3));
+                parada.setIcono(cursor.getString(4));
+                return parada;
             }
             else
                 return null;
@@ -77,41 +77,82 @@ public class ModeloAdo {
         return alumnos;
     }
 
-    public Modelo insertarAlumno(String id, String nombre, String direccion, String telefono, String email){
+    public Modelo insertarAlumno(String id, String titulo, String ultmimaactualizacion, String coordenadas, String icono){
         try (SQLiteDatabase db = Util.abrirBD(context, Util.DBNAME)) {
             ContentValues cv = new ContentValues();
-            cv.put("id", id);
-            cv.put(NOMBRE, nombre);
-            cv.put("direccion", direccion);
-            cv.put("telefono", telefono);
-            cv.put("email", email);
+            cv.put(ID, id);
+            cv.put("titulo", titulo);
+            cv.put("utlimaactualizacion", ultmimaactualizacion);
+            cv.put("coordenadas", coordenadas);
+            cv.put("icono", icono);
 
 
-            Modelo alumno = new Modelo();
-            alumno.setId(id);
-            alumno.setTitulo(nombre);
-            alumno.setUltimaactualizacion(direccion);
-            alumno.setCoordenadas(telefono);
-            alumno.setIcono(email);
+            Modelo parada = new Modelo();
+            parada.setId(id);
+            parada.setTitulo(titulo);
+            parada.setUltimaactualizacion(ultmimaactualizacion);
+            parada.setCoordenadas(coordenadas);
+            parada.setIcono(icono);
 
-            return alumno;
+            return parada;
         }
     }
 
-    public void actualizarAlumno(Modelo alumno){
+    public void actualizarAlumno(Modelo parada){
         try (SQLiteDatabase db = Util.abrirBD(context, Util.DBNAME)){
             ContentValues cv = new ContentValues();
-            cv.put(NOMBRE, alumno.getTitulo());
-            cv.put("direccion", alumno.getUltimaactualizacion());
-            cv.put("telefono", alumno.getCoordenadas());
-            cv.put("email", alumno.getIcono());
-            db.update("alumnos", cv, "id = ?", new String[]{String.valueOf(alumno.getId())});
+            cv.put(ID, parada.getTitulo());
+            cv.put("ultimaactualizacion", parada.getUltimaactualizacion());
+            cv.put("coordenadas", parada.getCoordenadas());
+            cv.put("icono", parada.getIcono());
+            db.update("alumnos", cv, "id = ?", new String[]{String.valueOf(parada.getId())});
         }
     }
 
-    public void borrarAlumno(Modelo alumno){
+    public void borrarAlumno(Modelo parada){
         try (SQLiteDatabase db = Util.abrirBD(context, Util.DBNAME)){
-            db.delete("alumnos", "id = ?", new String[] {String.valueOf(alumno.getId())});
+            db.delete("alumnos", "id = ?", new String[] {String.valueOf(parada.getId())});
         }
+    }
+
+    public List<Modelo> update(){
+        List<Modelo> parada = new ArrayList<>();
+        String sql = "UPDATE alumnos SET alumnos = ?";
+
+        try (SQLiteDatabase db = Util.abrirBD(context, Util.DBNAME)){
+            Cursor cursor = db.rawQuery(sql, null);
+
+            rellenarAlumnos(cursor, parada);
+        }
+
+        return parada;
+    }
+
+    public List<Modelo> createTable(){
+        List<Modelo> alumnos = new ArrayList<>();
+        String sql = "CREATE TABLE databasename(String parada)";
+
+        try (SQLiteDatabase db = Util.abrirBD(context, Util.DBNAME)){
+            Cursor cursor = db.rawQuery(sql, null);
+
+            rellenarAlumnos(cursor, alumnos);
+        }
+
+        return alumnos;
+    }
+
+    public List<Modelo> busccar(){
+        List<Modelo> alumnos = new ArrayList<>();
+        String sql = "SELECT * FROM nombre_tabla WHERE id LIKE '%CAMPO_BUSCAR%' OR titulo LIKE '%CAMPO_BUSCAR%'" +
+                "OR ultimaactualizacion LIKE '%CAMPO_BUSCAR%' OR coordenadas LIKE '%CAMPO_BUSCAR%' OR " +
+                "icono LIKE '%CAMPO_BUSCAR%'";
+
+        try (SQLiteDatabase db = Util.abrirBD(context, Util.DBNAME)){
+            Cursor cursor = db.rawQuery(sql, null);
+
+            rellenarAlumnos(cursor, alumnos);
+        }
+
+        return alumnos;
     }
 }
