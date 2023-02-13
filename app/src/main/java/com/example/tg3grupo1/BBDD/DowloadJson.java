4a70d92d304581c1256f6f0e87b0c0d4c32da735
dani <danielalvarorio@gmail.com>
2023-02-13 19:34:28 +0100

    configuracion basica de fragments y de inicio
    la descarga esta echa pero falta algo de maquetacion con el progress dialog, y tambien hacer la funcionalidad de home, actualizar y buscar

--- app/src/main/java/com/example/tg3grupo1/BBDD/DowloadJson.java
+++ app/src/main/java/com/example/tg3grupo1/BBDD/DowloadJson.java
@@ -1,16 +1,35 @@
 package com.example.tg3grupo1.BBDD;
+
+import android.content.Context;
 import android.os.AsyncTask;
+import android.view.View;
+
+import com.example.tg3grupo1.Modelo.Modelo;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DowloadJson extends AsyncTask<String, Void, String> {
+    private static Context CONTEXT;
+    private static String result = "";
+    public static ArrayList<Modelo> modelos = new ArrayList<>();
+
+    public static void metercosas(Context contexto, View view) {
+
+    }
+
     @Override
     protected String doInBackground(String... urls) {
-        String result = "";
+        result = "";
         URL url;
         HttpURLConnection urlConnection = null;
         try {
@@ -24,6 +43,7 @@ public class DowloadJson extends AsyncTask<String, Void, String> {
                 result += current;
                 data = reader.read();
             }
+            cargarModelo();
             return result;
         } catch (Exception e) {
             e.printStackTrace();
@@ -33,5 +53,33 @@ public class DowloadJson extends AsyncTask<String, Void, String> {
                 urlConnection.disconnect();
             }
         }
+    }
+
+    public void cargarModelo() {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(result, JsonObject.class);
+        JsonElement idElement = jsonObject.get("result");
+        JsonArray lista = idElement.getAsJsonArray();
+        List<JsonObject> objeto = new ArrayList<>();
+        for (int i = 0; i < lista.size(); i++) {
+            objeto.add((JsonObject) lista.get(i));
+        }
+        List<JsonObject> geome = new ArrayList<>();
+        for (int i = 0; i < objeto.size(); i++) {
+            geome.add((JsonObject) objeto.get(i).get("geometry"));
+            //geome.get(i).get("coordinates");
+
+            modelos.add(new Modelo(objeto.get(i).get("id").toString(),
+                    objeto.get(i).get("title").toString(),
+                    objeto.get(i).get("lastUpdated").toString(),
+                    geome.get(i).get("coordinates").toString(),
+                    objeto.get(i).get("icon").toString()
+            ));
+        }
+//        ModeloHelper modeloHelper = new ModeloHelper(this);
+//        modeloHelper.getWritableDatabase();
+//        ModeloAdo modeloAdo = new ModeloAdo(this);
+//        modeloAdo.rellenarAlumnos(flkjflkj, modelos);
+
     }
 }
--- app/src/main/java/com/example/tg3grupo1/Vistas/Inicio.java
+++ app/src/main/java/com/example/tg3grupo1/Vistas/Inicio.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import com.example.tg3grupo1.BBDD.DowloadJson;
 import com.example.tg3grupo1.BBDD.ModeloAdo;
 import com.example.tg3grupo1.BBDD.ModeloHelper;
+import com.example.tg3grupo1.Fragments.ContenidoGeneral;
 import com.example.tg3grupo1.Modelo.Modelo;
 import com.example.tg3grupo1.R;
 import com.google.gson.Gson;
@@ -28,51 +29,48 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class Inicio extends AppCompatActivity {
-    private static String contenido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.fondoAplicacion);
+        //setTheme(R.style.fondoAplicacion);
         setContentView(R.layout.activity_inicio);
 
+        showFragment(ContenidoGeneral.newInstance(this));
 
+//        DowloadJson.metercosas(this, );
         DowloadJson json = new DowloadJson();
+
         try {
-            contenido = json.execute("https://www.zaragoza.es/sede/servicio/urbanismo-infraestructuras/equipamiento/parada-taxi.json?rf=html&srsname=wgs84&start=0&rows=1000&distance=500").get();
+            json.execute("https://www.zaragoza.es/sede/servicio/urbanismo-infraestructuras/equipamiento/parada-taxi.json?rf=html&srsname=wgs84&start=0&rows=1000&distance=500").get();
         } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();
         }
-        cargarodelo();
+//        cargarodelo();
     }
 
-    private void cargarodelo() {
-        Gson gson = new Gson();
-        JsonObject jsonObject = gson.fromJson(contenido, JsonObject.class);
-        JsonElement idElement = jsonObject.get("result");
-        JsonArray lista = idElement.getAsJsonArray();
-        List<JsonObject> objeto = new ArrayList<>();
-        for (int i = 0; i < lista.size(); i++) {
-            objeto.add((JsonObject) lista.get(i));
-        }
-        List<JsonObject> geome = new ArrayList<>();
-        List<Modelo> modelos = new ArrayList<>();
-        for (int i = 0; i < objeto.size(); i++) {
-            geome.add((JsonObject) objeto.get(i).get("geometry"));
-            geome.get(i).get("coordinates");
-            modelos.add(new Modelo(
-                    objeto.get(i).get("id").toString(),
-                    objeto.get(i).get("title").toString(),
-                    objeto.get(i).get("lastUpdated").toString(),
-                    geome.get(i).get("coordinates").toString(),
-                    objeto.get(i).get("icon").toString()
-            ));
-        }
-//        ModeloHelper modeloHelper = new ModeloHelper(this);
-//        modeloHelper.getWritableDatabase();
-//        ModeloAdo modeloAdo = new ModeloAdo(this);
-//        modeloAdo.rellenarAlumnos(flkjflkj, modelos);
-    }
+//    private void cargarodelo() {
+//        Gson gson = new Gson();
+//        JsonObject jsonObject = gson.fromJson(contenido, JsonObject.class);
+//        JsonElement idElement = jsonObject.get("result");
+//        JsonArray lista = idElement.getAsJsonArray();
+//        List<JsonObject> objeto = new ArrayList<>();
+//        for (int i = 0; i < lista.size(); i++) {
+//            objeto.add((JsonObject) lista.get(i));
+//        }
+//        List<JsonObject> geome = new ArrayList<>();
+//        List<Modelo> modelos = new ArrayList<>();
+//        for (int i = 0; i < objeto.size(); i++) {
+//            geome.add((JsonObject) objeto.get(i).get("geometry"));
+//            //geome.get(i).get("coordinates");
+//
+//            modelos.add(new Modelo(objeto.get(i).get("id").toString(), objeto.get(i).get("title").toString(), objeto.get(i).get("lastUpdated").toString(), geome.get(i).get("coordinates").toString(), objeto.get(i).get("icon").toString()));
+//        }
+////        ModeloHelper modeloHelper = new ModeloHelper(this);
+////        modeloHelper.getWritableDatabase();
+////        ModeloAdo modeloAdo = new ModeloAdo(this);
+////        modeloAdo.rellenarAlumnos(flkjflkj, modelos);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -93,10 +91,10 @@ public class Inicio extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    public void showFragment(Fragment fragment) {
-//        getSupportFragmentManager()
-//                .beginTransaction()
-//                .replace(R.id.contenedor, fragment)
-//                .commit();
-//    }
+    public void showFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.contenedor, fragment)
+                .commit();
+    }
 }
