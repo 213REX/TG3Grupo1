1a31008da62bdf01e451b6501661661917deff70
Nicolás Del Amo <nico.delamo.perez@gmail.com>
2023-02-14 17:27:26 +0100

    crito en ti confio

--- app/src/main/java/com/example/tg3grupo1/Vistas/Splash.java
+++ app/src/main/java/com/example/tg3grupo1/Vistas/Splash.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.example.tg3grupo1.BBDD.DownloadJson;
 import com.example.tg3grupo1.R;
+
+import java.util.concurrent.ExecutionException;
 
 public class Splash extends AppCompatActivity {
 
@@ -16,15 +19,24 @@ public class Splash extends AppCompatActivity {
         setTheme(R.style.fondoInicio);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        //pasamos el contexto y despues creamos un objeto de la clase downloadJson
+        //para asi ejecutarlo pasandole la url y que descargue todos los datos y que los
+        //visualice
+        DownloadJson.metercosas(this);
+        DownloadJson json = new DownloadJson();
+        try {
+            json.execute("https://www.zaragoza.es/sede/servicio/urbanismo-infraestructuras/equipamiento/parada-taxi.json?rf=html&srsname=wgs84&start=0&rows=1000&distance=500%22").get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
         //el siguiente codigo permite que el splash cambie directamente
         //a la main activity tras 5 segundos
         Handler handler = new Handler(Looper.myLooper());
-        handler.postDelayed(()->{
+        handler.postDelayed(() -> {
 
             Intent intent = new Intent(getApplicationContext(), Inicio.class);
             startActivity(intent);
             finish();
-        },5000);
+        }, 5000);
     }
 }
